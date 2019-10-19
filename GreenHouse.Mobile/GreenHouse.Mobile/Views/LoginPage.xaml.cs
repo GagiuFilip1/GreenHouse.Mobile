91c0417504ab9edfd1eaffea56b49bd73b54596a
hohaaa <hoha_mihai@yahoo.com>
2019-10-19 23:21:50 +0300

    GHM-2 Added LoginPage and SignupPage

---
+++ GreenHouse.Mobile/GreenHouse.Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenHouse.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        
+        void SigninProcedure(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new MainPage ();
+        }
+
+        void SignupProcedure(object sender, EventArgs e)
+        {
+            Navigation.PushAsync (new SignupPage ());
+            //Application.Current.MainPage = new SignupPage ();
+        }
+    }
+}
---
+++ GreenHouse.Mobile/GreenHouse.Mobile/Views/SignupPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenHouse.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignupPage : ContentPage
+    {
+        public SignupPage()
+        {
+            InitializeComponent();
+        }
+        
+        public void SignupProcedure(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
